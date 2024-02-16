e07c1e7ef3f4a9d6c1cf47aab3581b7cd240da84
Jayant pawar <pawarjayant80@gmail.com>
2024-02-16 18:23:07 +0530

    initial

---
+++ Backend/index.js
@@ -0,0 +1,24 @@
+const express= require('express')
+const app =express();
+
+const path = require('path')
+const envPath=path.resolve(__dirname,"../.env")
+const dotenv=require('dotenv');
+dotenv.config({path:envPath})
+require('./db')
+const PORT=process.env.PORT;
+const userRoutes=require('./routes/userRoutes')
+const taskRoutes=require('./routes/taskRoutes')
+
+
+app.use(express.json())
+app.use('/users',userRoutes)
+app.use('/task',taskRoutes)
+
+app.get('/',(req,res)=>{
+    res.json({message:'Task Manager API is working'})
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}.`)
+})
